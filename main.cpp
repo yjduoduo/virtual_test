30d0584932a911907d640c58ac614219e74418ad
yjduoduo <wxjlmr@126.com>
2014-04-20 09:00:09 +0800

    this is first

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    while(1);
+    return 0;
+}
+
