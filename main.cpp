0b8b722109ea835563ab299a28fb4e7ee396a3bc
yjduoduo <wxjlmr@126.com>
2014-04-20 10:13:21 +0800

    add func to caculate binary nums

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int func(int x)
+{
+    int countx =0;
+    while(x)
+    {
+        countx ++;
+        x = x&(x-1);
+    }
+    return countx;
+}
+
 int main()
 {
     cout << "Hello World!" << endl;
@@ -12,6 +23,7 @@ int main()
     cout << "Hello World!" << endl;
     cout << "Hello World!" << endl;
     cout << "Hello World!" << endl;
+    cout << "func(999)"<<func(99999)<<endl;
     while(1);
     return 0;
 }
